b5e3e6caacbd6b54beb6d578a8fbc20557cf9308
xzhuo <xzhuo@xzhuo-home>
2019-01-06 03:55:59 +0800

    1

---
+++ sboot21-1/src/main/java/com/sboot/Sboot211Application.java
@@ -0,0 +1,14 @@
+package com.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sboot211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sboot211Application.class, args);
+	}
+
+}
+

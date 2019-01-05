d3ea620962aca5a7cf2b33a9439a99a0f509971a
xzhuo <xzhuo@xzhuo-home>
2019-01-06 04:09:50 +0800

    1

--- sboot21-1/src/test/java/com/sboot/SbootApplicationTests.java
+++ sboot21-1/src/test/java/com/sboot/SbootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Sboot211ApplicationTests {
+public class SbootApplicationTests {
 
 	@Test
 	public void contextLoads() {
